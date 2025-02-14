9b254988999bcf6a40f49689f34b5e17629bc6c9
llanito7361 <llanito.code.log@gmail.com>
2025-02-14 14:54:27 -0300

    /product/:id page

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,8 +1,69 @@
-
+import { useContext, useState, useEffect } from "react";
+import { ShopContext } from "../context/ShopContext";
+import Title from "../components/Title";
+import { assets } from "../assets/assets";
 const Cart = () => {
-  return (
-    <div>Cart</div>
-  )
-}
+  const { products, currency, cartItems,updateQuantity } = useContext(ShopContext);
+  const [cartData, setCartData] = useState([]);
 
-export default Cart
+  useEffect(() => {
+    const tempData = [];
+    for (const items in cartItems) {
+      for (const item in cartItems[items]) {
+        if (cartItems[items][item] > 0) {
+          tempData.push({
+            _id: items,
+            size: item,
+            quantity: cartItems[items][item],
+          });
+        }
+      }
+    }
+    console.log("tumama", tempData);
+    setCartData(tempData);
+    // 3h46m
+  }, [cartItems]);
+
+  return (
+    <div className="border-t pt-14">
+      <div className="text-2xl mb-3">
+        <Title text1={"YOUR"} text2={"CART"} />
+      </div>
+
+      <div>
+        {cartData.map((item, index) => {
+          const productData = products.find(
+            (product) => product._id === item._id
+          );
+
+          return (
+            <div
+              key={index}
+              className="py-4 border-t border-b text-gray-700 grid grid-cols-[4fr_0.5fr_0.5fr] sm:grid-cols-[4fr_2fr_0.5fr] items-center"
+            >
+              <div className="flex items-start gap-6">
+                <img
+                  className="w-16 sm:w-20"
+                  src={productData.image[0]}
+                  alt=""
+                />
+                <div>
+                  <p className="text-xs sm:text-lg font-medium">{productData.name}</p>
+                  <div className="flex items-center gap-5 mt-2">
+                    <p>{currency}{productData.price}</p>
+                    <p className="px-2 sm:px-3 sm:py-1 border bg-slate-50">{item.size}</p>
+                  </div>
+                </div>
+              </div>
+              <input onClick={(e)=>e.target.value === '' || e.target.value === '0' ? null : updateQuantity(item._id,item.size,Number(e.target.value))} className="border text-center max-h-8 max-w-10 sm:max-w-20 px-1 sm:px-2 m-auto " type="number" min={1} defaultValue={item.quantity}  />
+              <img onClick={()=>{updateQuantity(item._id,item.size,0)}} src={assets.bin_icon} alt="assets.bin_icon" className="w-4 sm:w-5 sm: w-5 cursor-pointer" />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
+// 4h4m20s
